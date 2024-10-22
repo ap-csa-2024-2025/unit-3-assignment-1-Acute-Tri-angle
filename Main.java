350c7539e8a18fae62a620db845759621a291241
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-10-22 10:58:48 -0700

    werewrewrwerwerwerwe

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main
 {
   public static void main(String[] args)
   {
     // write solutions to problems here
+    Rectangle rect1 = new Rectangle(4,6);
+    Rectangle rect2 = new Rectangle(12,8);
+    boolean isRotated = ((rect1.getLength() == rect2.getWidth()) && (rect1.getWidth() == rect2.getLength()));
+    boolean isCongruent = (isRotated || rect1.equals(rect2));
+    boolean isSimilar = (isCongruent || (rect1.getLength() / rect2.getLength() == rect1.getWidth() / rect2.getWidth()));
+
+    System.out.println(isRotated + " " + isCongruent + " " + isSimilar);
   }
 }
